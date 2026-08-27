e8502d395c56adb6f64c5ac1802f106e6aa0b3e8
Patrick Pessanha <patrickpessanha@MacBook-Air-de-Patrick.local>
2026-08-27 12:50:42 -0400

    chore: connect app to mysql

--- src/main/java/com/patrick/Main.java
+++ src/main/java/com/patrick/Main.java
@@ -1,17 +1,13 @@
 package com.patrick;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.patrick.db.DB;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
     }
 }
---
+++ src/main/java/com/patrick/db/DbExecption.java
@@ -0,0 +1,7 @@
+package com.patrick.db;
+
+public class DbExecption extends RuntimeException {
+    public DbExecption(String message) {
+        super(message);
+    }
+}
