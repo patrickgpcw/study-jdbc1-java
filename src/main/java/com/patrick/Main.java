de6b7ce6497b49eb337017075e264faa7710312b
Patrick Pessanha <patrickpessanha@MacBook-Air-de-Patrick.local>
2026-09-01 16:28:42 -0400

    feat: add seller entity

--- src/main/java/com/patrick/Main.java
+++ src/main/java/com/patrick/Main.java
@@ -2,10 +2,17 @@ package com.patrick;
 
 import com.patrick.db.DB;
 import com.patrick.model.entities.Department;
+import com.patrick.model.entities.Seller;
+
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department dp = new Department(1, "Book");
-        System.out.println(dp);
+
+        Seller s = new Seller(1, "Patrick", "patrick@pessanha.com", new Date(), 3000.0, dp);
+
+        System.out.println(s);
     }
 }
