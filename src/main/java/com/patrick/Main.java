cca5ee8e16582916961928f0e526cba6459268ca
patrickpessanha <patrick.surfista@hotmail.com>
2026-09-08 14:47:41 -0400

    feat: implement inset on SellerDaoJDBC

--- src/main/java/com/patrick/Main.java
+++ src/main/java/com/patrick/Main.java
@@ -2,17 +2,37 @@ package com.patrick;
 
 import com.patrick.model.dao.FactoryDao;
 import com.patrick.model.dao.SellerDao;
+import com.patrick.model.entities.Department;
+import com.patrick.model.entities.Seller;
+
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
+
+        System.out.println("=== TEST 3: seller insert ===");
+        Department department = new Department(1, "Computers");
+        Seller seller = new Seller(null,
+                "Marcelo Junior",
+                "marcelo@junior.com",
+                Date.valueOf(LocalDate.of(1995, 12, 27)),
+                3000.0,
+                department);
+
         SellerDao sellerDao = FactoryDao.createSellerDao();
+        sellerDao.insert(seller);
+
+        System.out.println( "The seller " +seller.getName() + " was inserted!");
+
 
 
 //        System.out.println("=== TEST 1: seller findById ===");
 //        Seller s = sellerDao.findById(8);
 //        System.out.println(s);
-        System.out.println("=== TEST 2: seller delete ===");
+//        System.out.println("=== TEST 2: seller delete ===");
+
 
         sellerDao.delete(1);
     }
--- src/main/java/com/patrick/model/impl/SellerDaoJDBC.java
+++ src/main/java/com/patrick/model/impl/SellerDaoJDBC.java
@@ -5,10 +5,7 @@ import com.patrick.model.dao.SellerDao;
 import com.patrick.model.entities.Department;
 import com.patrick.model.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
@@ -20,6 +17,28 @@ public class SellerDaoJDBC implements SellerDao {
     }
     @Override
     public void insert(Seller seller) {
+        String sql = "INSERT INTO seller"
+                + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                + "VALUE "
+                + "(?,?,?,?,?)";
+
+        try(PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, seller.getName());
+            ps.setString(2, seller.getEmail());
+            ps.setDate(3, new Date(seller.getBirthDate().getTime()));
+            ps.setDouble(4, seller.getBaseSalary());
+            ps.setInt(5, seller.getDepartment().getId());
+            int rowAffects = ps.executeUpdate();
+            if (rowAffects>0) {
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    seller.setId(id);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DbException("Error to insert seller: ", e);
+        }
 
     }
 
